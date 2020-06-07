35f50e0f91db8af40cb737b21956117a4929f23b
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-06-07 15:09:17 -0800

    updated app.css and app.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      hello world
+    <div className="header">
+      <h1>This is first React App deploy on Surge</h1>
+      <h2>My name is <strong>Bilal Ahmad</strong>.</h2>
+      <p>I am student of SMIT</p>
     </div>
   );
 }
