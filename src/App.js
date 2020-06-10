d3f98b17a055f72260684d7ebd0275171bdcca55
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-06-10 13:50:03 -0800

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-
 function App() {
   return (
-    <div className="header">
-      <h1>This is first React App deploy on Surge</h1>
-      <h2>My name is <strong>Bilal Ahmad</strong>.</h2>
-      <p>I am student of SMIT</p>
+    <div className='header'>
+      <h1>Application For COVID-19</h1>
+      <p>COVID-19 is a type of virus spreads all over the world</p>
+      <button>Click For Detail</button>
     </div>
   );
 }
