20da0a9efcdcb2a1ac6da3c0423c5c992bc33053
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-06-11 19:48:30 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ function App() {
   let [count, setcount] = useState(1);
   let [isMorning, setMorning] = useState(true);
 
+
   return (
-    <div className={`header ${ isMorning ? 'daylight' : '' }`}>
+    <div className={`header ${isMorning ? 'daylight':'' } `}>
       <h1>Good {isMorning ? 'Morning' : 'Night'}</h1>
       <Message counter={count} />
-      <button onClick={()=> setcount(count + 1)}>Update Counter</button>
-      <button onClick={()=>setMorning(!isMorning)}>Change Mood</button>
+      <button onClick={() => setcount(count + 1)}>Plus Counter Value</button>
+      <button onClick={() => setcount(count - 1)}>Minus Counter Value</button>
+      <button onClick={() => setMorning(!isMorning)}>Change MOod View</button>
     </div>
   );
 }
